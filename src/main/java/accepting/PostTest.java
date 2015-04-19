1e9fac1c00914e0d8c12c4ed5a0ca99a59534830
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-19 12:42:24 +0200

    second test for post class toString

--- src/main/java/accepting/PostTest.java
+++ src/main/java/accepting/PostTest.java
@@ -13,4 +13,12 @@ public class PostTest {
 
         assertThat(post.toString(), is(""));
     }
+
+    @Test
+    public void createdWithFilledMessageItPrintsThatMessage() {
+        Message message = new Message("I love the weather today");
+        Post post = new Post(message);
+
+        assertThat(post.toString(), is("I love the weather today"));
+    }
 }
