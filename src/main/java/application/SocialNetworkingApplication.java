f3adadbc658f9de1c66ea9aadb119baaaf480946
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-22 17:21:44 +0200

    Implemented Basic SocialNetworkingApplication

--- src/main/java/application/SocialNetworkingApplication.java
+++ src/main/java/application/SocialNetworkingApplication.java
@@ -1,19 +1,33 @@
 package application;
 
 import java.time.Clock;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import accepting.MessageParser;
+import accepting.Post;
+import accepting.Posts;
+import accepting.SocialTime;
+
 public class SocialNetworkingApplication {
+    private Posts posts = new Posts();
+    private List<String> output = new ArrayList<>();
+    private Clock clock;
 
     public SocialNetworkingApplication(Clock clock) {
+        this.clock = clock;
     }
 
     public void accept(String message) {
+        if (message.equals("Alice"))
+            output.addAll(posts.printPosts(new SocialTime(LocalDateTime.now(clock))));
+        else
+            posts.addPost(new Post(new MessageParser().parse(message), new SocialTime(LocalDateTime.now(clock))));
     }
 
     public List<String> getOutput() {
-        return new ArrayList<>();
+        return output;
     }
 
 }
