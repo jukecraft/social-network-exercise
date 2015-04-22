908209081d6395df723954fd20e661b0db13b1ba
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-22 15:44:22 +0200

    Implemented delegation to socialtime and aggregation with message text

--- src/main/java/accepting/Post.java
+++ src/main/java/accepting/Post.java
@@ -3,13 +3,15 @@ package accepting;
 public class Post {
 
     private Message message;
+    private SocialTime postingTime;
 
     public Post(Message message, SocialTime postingTime) {
         this.message = message;
+        this.postingTime = postingTime;
     }
 
     public String printTimestamp(SocialTime postingTime) {
-        return message.toString() + " (5 minutes ago)";
+        return message.toString() + " (" + this.postingTime.printTimestamp(postingTime) + ")";
     }
 
 }
