e0e537d1c9ba397d3c30a0271da2d6c986e21d66
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-22 16:54:20 +0200

    implemented printing for one post

--- src/main/java/accepting/Posts.java
+++ src/main/java/accepting/Posts.java
@@ -1,17 +1,21 @@
 package accepting;
 
-import static java.util.Collections.emptyList;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Posts {
+    private List<Post> posts = new ArrayList<>();
 
     public List<String> printPosts(SocialTime socialTime) {
-        return emptyList();
+        if (posts.isEmpty())
+            return Collections.emptyList();
+        return Arrays.asList(posts.get(0).printTimestamp(socialTime));
     }
 
     public void addPost(Post create) {
-        // TODO Auto-generated method stub
+        posts.add(create);
 
     }
 
