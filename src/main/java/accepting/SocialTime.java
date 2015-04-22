c4612a2275a34b4d423f294b309a97d7aaddc4bb
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-22 22:48:53 +0200

    Refactoring for clarity

--- src/main/java/accepting/SocialTime.java
+++ src/main/java/accepting/SocialTime.java
@@ -1,11 +1,16 @@
 package accepting;
 
+import static java.text.MessageFormat.format;
 import static java.time.Duration.between;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class SocialTime implements Comparable<SocialTime> {
+    private static final String UNIT_ONE_MINUTE = "minute";
+    private static final String UNIT_MULTIPLE_MINUTES = "minutes";
+    private static final String TIMESTAMP_PATTERN = "{0} {1} ago";
+
     private LocalDateTime timestamp;
 
     public SocialTime(LocalDateTime timestamp) {
@@ -14,14 +19,17 @@ public class SocialTime implements Comparable<SocialTime> {
 
     public String printTimestamp(SocialTime timeOfPrinting) {
         Duration timePassed = between(this.timestamp, timeOfPrinting.timestamp);
-        long numberOfMinutes = timePassed.toMinutes();
-        return numberOfMinutes + " " + getTimestring(numberOfMinutes) + " ago";
+        return printTimestamp(timePassed.toMinutes());
     }
 
-    private String getTimestring(long numberOfMinutes) {
+    private String printTimestamp(long numberOfMinutes) {
+        return format(TIMESTAMP_PATTERN, numberOfMinutes, getUnit(numberOfMinutes));
+    }
+
+    private String getUnit(long numberOfMinutes) {
         if (numberOfMinutes > 1)
-            return "minutes";
-        return "minute";
+            return UNIT_MULTIPLE_MINUTES;
+        return UNIT_ONE_MINUTE;
     }
 
     @Override
