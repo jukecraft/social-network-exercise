ee79e6403f225c751e2504c5096ae26126dda0d9
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-22 16:36:37 +0200

    fake implementation for printPosts

--- src/main/java/accepting/Posts.java
+++ src/main/java/accepting/Posts.java
@@ -1,12 +1,12 @@
 package accepting;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Posts {
 
     public List<String> printPosts() {
-        // TODO Auto-generated method stub
-        return null;
+        return Collections.emptyList();
     }
 
 }
