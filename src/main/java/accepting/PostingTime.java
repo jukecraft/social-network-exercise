e6c753ac479ad0d0a0d66b07db293adeb0976f91
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-22 15:14:15 +0200

    Implemented timestamp creation

--- src/main/java/accepting/PostingTime.java
+++ src/main/java/accepting/PostingTime.java
@@ -1,5 +1,8 @@
 package accepting;
 
+import static java.time.Duration.between;
+
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class PostingTime {
@@ -10,10 +13,12 @@ public class PostingTime {
     }
 
     public PostingTime plusMinutes(int numberOfMinutes) {
-        return this;
+        return new PostingTime(postingTime.plusMinutes(numberOfMinutes));
     }
 
     public String printTimestamp(PostingTime postingTime) {
-        return "5 minutes ago";
+        Duration timePassed = between(this.postingTime, postingTime.postingTime);
+        long numberOfMinutes = timePassed.toMinutes();
+        return numberOfMinutes + " minutes ago";
     }
 }
