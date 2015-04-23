c5d956c5640ef7b3283ed52d833bc7d800f7f573
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-23 12:25:11 +0200

    implemented storing of posts in a timeline

--- src/main/java/accepting/Timelines.java
+++ src/main/java/accepting/Timelines.java
@@ -2,8 +2,16 @@ package accepting;
 
 public class Timelines {
 
+    private Timeline timeline = new Timeline();
+
     public Timeline getPostsFor(String username) {
-        return new Timeline();
+        return timeline;
+    }
+
+    public void post(String string, Post post) {
+        timeline.addPost(post);
+        // TODO Auto-generated method stub
+
     }
 
 }
