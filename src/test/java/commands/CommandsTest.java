c577e2cf404062343f599191ea6e7ee1fd50b389
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-25 20:02:24 +0200

    Added Test for Output Collection

--- src/test/java/commands/CommandsTest.java
+++ src/test/java/commands/CommandsTest.java
@@ -3,11 +3,15 @@ package commands;
 import static accepting.UserBuilder.aUser;
 import static accepting.builder.SocialTimeBuilder.aTime;
 import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,8 +21,11 @@ import accepting.User;
 
 public class CommandsTest {
     private static final String USERNAME = "Alice";
-    private static final String COMMAND = "test command";
+    private static final User EXPECTED_USER = aUser().withName(USERNAME).create();
+    private static final String COMMAND = " test command";
     private static final SocialTime TIME = aTime().create();
+    private static final String OUTPUT1 = "output";
+    private static final String OUTPUT2 = "another output";
 
     private Command applicableCommand = mock(Command.class);
     private Command notApplicableCommand = mock(Command.class);
@@ -32,12 +39,21 @@ public class CommandsTest {
     @Test
     public void givenMultipleCommandsWhenTheyAreExecutedThenOnlyTheApplicableAreExecutedWithTheExtractedUser() {
         Commands commands = new Commands(asList(applicableCommand, notApplicableCommand));
-        User expectedUser = aUser().withName(USERNAME).create();
 
         commands.execute(USERNAME + COMMAND, TIME);
 
-        verify(applicableCommand).executeCommand(expectedUser, COMMAND, TIME);
+        verify(applicableCommand).executeCommand(EXPECTED_USER, COMMAND, TIME);
         verify(notApplicableCommand, never()).executeCommand(any(User.class), any(String.class), any(SocialTime.class));
+    }
+
+    @Test
+    public void givenMultipleCommandsTheOutputIsCollected() {
+        when(applicableCommand.executeCommand(EXPECTED_USER, COMMAND, TIME)).thenReturn(asList(OUTPUT1, OUTPUT2));
+        Commands commands = new Commands(asList(applicableCommand, applicableCommand));
+
+        List<String> output = commands.execute(USERNAME + COMMAND, TIME);
+
+        assertThat(output, contains(OUTPUT1, OUTPUT2, OUTPUT1, OUTPUT2));
     }
 
 }
