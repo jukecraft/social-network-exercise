99aaedcaef6e1a101cb949fab9bc0698c11d61f5
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-25 20:02:01 +0200

    Implemented Execution of commands

--- src/main/java/commands/Commands.java
+++ src/main/java/commands/Commands.java
@@ -1,17 +1,30 @@
 package commands;
 
+import static java.util.Collections.emptyList;
+
 import java.util.List;
 
 import accepting.SocialTime;
+import accepting.User;
 
 public class Commands {
+    private List<Command> commands;
+    private static final String COMMAND_SEPERATOR = " ";
 
     public Commands(List<Command> commands) {
 
+        this.commands = commands;
     }
 
     public List<String> execute(String command, SocialTime time) {
+        String[] commandParts = command.split(COMMAND_SEPERATOR);
+        User user = new User(commandParts[0]);
+        String commandWithoutUser = command.substring(commandParts[0].length());
 
-        return null;
+        for (Command command2 : commands) {
+            if (command2.isApplicable(commandWithoutUser))
+                command2.executeCommand(user, commandWithoutUser, time);
+        }
+        return emptyList();
     }
 }
