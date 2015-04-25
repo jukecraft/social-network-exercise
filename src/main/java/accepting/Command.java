fe37bea87362a8e1eb806f25acb675712f3d7495
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-25 17:17:44 +0200

    implemented parsing of user

--- src/main/java/accepting/Command.java
+++ src/main/java/accepting/Command.java
@@ -2,11 +2,15 @@ package accepting;
 
 public class Command {
 
+    private String user;
+
     public Command(String command) {
+        String[] commandParts = command.split(" ");
+        user = commandParts[0];
     }
 
     public String getUser() {
-        return "Alice";
+        return user;
     }
 
 }
