1b30829942338e365cf9f443287df9358644e052
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-26 22:23:31 +0200

    added test with a follower

--- src/test/java/timeline/SocialNetworkTest.java
+++ src/test/java/timeline/SocialNetworkTest.java
@@ -1,5 +1,6 @@
 package timeline;
 
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -10,12 +11,25 @@ import java.util.List;
 import org.junit.Test;
 
 public class SocialNetworkTest {
+    private static final User ALICE = aUser().withName("Alice").create();
+    private static final User BOB = aUser().withName("Bob").create();;
+
     @Test
-    public void givenNoRegisteredFollowersThenItReturnsNoFollowers() {
+    public void givenFollowingWhenAskedForTheFollowingThenItReturnsNoFollowers() {
         SocialNetwork socialNetwork = new SocialNetwork();
 
         List<User> following = socialNetwork.getFollowing(aUser().create());
 
         assertThat(following, is(empty()));
+    }
+
+    @Test
+    public void givenOneRegisteredFollowingWhenAskedForTheFollowingThenItReturnsThatFollowers() {
+        SocialNetwork socialNetwork = new SocialNetwork();
+        socialNetwork.registerFollowing(ALICE, BOB);
+
+        List<User> following = socialNetwork.getFollowing(ALICE);
+
+        assertThat(following, contains(BOB));
     }
 }
