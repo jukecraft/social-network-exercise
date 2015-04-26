477c1be4eee22e3a91dae95ee5b34e6465b18002
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-26 22:28:59 +0200

    Implemented SocialNetwork

--- src/main/java/timeline/SocialNetwork.java
+++ src/main/java/timeline/SocialNetwork.java
@@ -1,16 +1,25 @@
 package timeline;
 
-import static java.util.Collections.emptyList;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SocialNetwork {
+    private Map<User, List<User>> network = new HashMap<>();
 
     public void registerFollowing(User follower, User following) {
+        List<User> followingForUser = get(follower);
+        followingForUser.add(following);
+        network.put(follower, followingForUser);
     }
 
     public List<User> getFollowing(User user) {
-        return emptyList();
+        return get(user);
+    }
+
+    private List<User> get(User user) {
+        return network.getOrDefault(user, new ArrayList<>());
     }
 
 }
