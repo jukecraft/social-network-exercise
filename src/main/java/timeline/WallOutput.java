7bbbd3d9090304119d1349cd0b69b0d1202ecc27
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-26 20:39:50 +0200

    fake implemented printing of wallOutput

--- src/main/java/timeline/WallOutput.java
+++ src/main/java/timeline/WallOutput.java
@@ -1,7 +1,12 @@
 package timeline;
 
+import static java.util.Collections.emptyList;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import time.SocialTime;
 
 public class WallOutput extends PostsOutput {
 
@@ -9,6 +14,11 @@ public class WallOutput extends PostsOutput {
 
     public void addPosts(User user, PostsOutput collectPosts) {
         timelines.put(user, collectPosts);
+    }
+
+    @Override
+    public List<String> getOutput(SocialTime printingTime) {
+        return emptyList();
     }
 
 }
