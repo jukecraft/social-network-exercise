dc0ba2d1d1107dc6768880f8f23a816ccda10d67
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-26 09:18:53 +0200

    removed unused feature

--- src/main/java/commands/User.java
+++ src/main/java/commands/User.java
@@ -4,13 +4,11 @@ import commons.SocialNetworkingValueObject;
 
 public class User extends SocialNetworkingValueObject {
 
+    @SuppressWarnings("unused")
     private final String name;
 
     public User(CommandParameter commandParameter) {
         this.name = commandParameter.getUser();
     }
 
-    public int lengthOfName() {
-        return name.length();
-    }
 }
