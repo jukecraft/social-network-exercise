a3366501309d8c085d8b06be9ecbeaa9cf8800c4
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-26 09:30:28 +0200

    introduced concept of FollowCommand

---
+++ src/test/java/commands/FollowCommand.java
@@ -0,0 +1,10 @@
+package commands;
+
+public class FollowCommand {
+
+    public boolean isApplicable(CommandParameter commandParameter) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
---
+++ src/test/java/commands/FollowCommandTest.java
@@ -0,0 +1,19 @@
+package commands;
+
+import static commands.CommandParameterBuilder.aCommand;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class FollowCommandTest {
+    @Test
+    public void itIsApplicableIfItContainsFollows() {
+        FollowCommand command = new FollowCommand();
+        CommandParameter commandParameter = aCommand().withCommand(" follows ").create();
+
+        boolean isApplicable = command.isApplicable(commandParameter);
+
+        assertThat(isApplicable, is(true));
+    }
+}
