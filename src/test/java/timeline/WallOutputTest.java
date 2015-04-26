7710a73cd5cee7683955aa6f2a645fbc17105e87
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-26 20:37:06 +0200

    added first test for WallOutput

---
+++ src/test/java/timeline/WallOutputTest.java
@@ -0,0 +1,30 @@
+package timeline;
+
+import static java.time.LocalDateTime.now;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static timeline.builder.SocialTimeBuilder.aTime;
+import static timeline.builder.WallOutputBuilder.anEmptyWallOutput;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.Test;
+
+import time.SocialTime;
+
+public class WallOutputTest {
+
+    private static final LocalDateTime PRINTING_TIMESTAMP = now();
+    private static final SocialTime PRINTING_TIME = aTime().withTimestamp(PRINTING_TIMESTAMP).create();
+
+    @Test
+    public void givenNoPostsWhenAskedToPrintTimelineItPrintsNothing() {
+        WallOutput output = anEmptyWallOutput().create();
+
+        List<String> printedTimeline = output.getOutput(PRINTING_TIME);
+
+        assertThat(printedTimeline, is(empty()));
+    }
+}
