f21aa4bf0ac59271428768b6af3b506dedc4e6a8
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-04-26 21:33:50 +0200

    added test for merging multiple timelines

--- src/test/java/timeline/WallOutputTest.java
+++ src/test/java/timeline/WallOutputTest.java
@@ -21,23 +21,32 @@ import time.SocialTime;
 public class WallOutputTest {
 
     private static final String SEPERATOR = " - ";
-    private static final String ALICE_USERNAME = "Alice";
     private static final LocalDateTime PRINTING_TIMESTAMP = now();
     private static final SocialTime PRINTING_TIME = aTime().withTimestamp(PRINTING_TIMESTAMP).create();
 
+    private static final User ALICE = aUser().withName("Alice").create();
     private static final String EARLIEST_MESSAGE = "I love the weather today";
     private static final String LATER_MESSAGE = "Good game though.";
-
-    private static final Post ALICES_FIRST_POST = aPost() //
+    private static final Post ALICES_EARLIEST_POST = aPost() //
         .withPostingTime(PRINTING_TIMESTAMP.minusMinutes(5)) //
         .withMessage(EARLIEST_MESSAGE) //
         .create();
-    private static final Post ALICES_SECOND_POST = aPost() //
+    private static final Post ALICES_LATER_POST = aPost() //
         .withPostingTime(PRINTING_TIMESTAMP.minusMinutes(1)) //
         .withMessage(LATER_MESSAGE) //
         .create();
 
-    private static final User ALICE = aUser().withName(ALICE_USERNAME).create();
+    private static final User CHARLIE = aUser().withName("Charlie").create();;
+    private static final String EARLIER_MESSAGE = "Damn! We lost!";
+    private static final String LATEST_MESSAGE = "I'm in New York today! Anyone want to have a coffee?";
+    private static final Post CHARLIES_EARLIER_POST = aPost() //
+        .withPostingTime(PRINTING_TIMESTAMP.minusMinutes(2)) //
+        .withMessage(EARLIER_MESSAGE) //
+        .create();
+    private static final Post CHARLIES_LATEST_POST = aPost() //
+        .withPostingTime(PRINTING_TIMESTAMP.minusSeconds(15)) //
+        .withMessage(LATEST_MESSAGE) //
+        .create();
 
     @Test
     public void givenNoPostsWhenAskedToPrintItPrintsNothing() {
@@ -51,8 +60,8 @@ public class WallOutputTest {
     @Test
     public void givenOneTimelinesWhenAskedToPrintItPrintsItWithTimestampsAndUsernameSortedLatestFirst() {
         PostsOutput alicesTimeline = anEmptyPostsOutput() //
-            .withPost(ALICES_FIRST_POST) //
-            .withPost(ALICES_SECOND_POST) //
+            .withPost(ALICES_EARLIEST_POST) //
+            .withPost(ALICES_LATER_POST) //
             .create();
 
         WallOutput output = anEmptyWallOutput() //
@@ -62,7 +71,32 @@ public class WallOutputTest {
         List<String> printedTimeline = output.print(PRINTING_TIME);
 
         assertThat(printedTimeline, contains( //
-            ALICE_USERNAME + SEPERATOR + LATER_MESSAGE + " (1 minute ago)", //
-            ALICE_USERNAME + SEPERATOR + EARLIEST_MESSAGE + " (5 minutes ago)"));
+            ALICE + SEPERATOR + LATER_MESSAGE + " (1 minute ago)", //
+            ALICE + SEPERATOR + EARLIEST_MESSAGE + " (5 minutes ago)"));
+    }
+
+    @Test
+    public void givenTwoTimelinesWhenAskedToPrintItPrintsThemWithTimestampsAndUsernameSortedLatestFirst() {
+        PostsOutput alicesTimeline = anEmptyPostsOutput() //
+            .withPost(ALICES_EARLIEST_POST) //
+            .withPost(ALICES_LATER_POST) //
+            .create();
+        PostsOutput charliesTimeline = anEmptyPostsOutput() //
+            .withPost(CHARLIES_EARLIER_POST) //
+            .withPost(CHARLIES_LATEST_POST) //
+            .create();
+
+        WallOutput output = anEmptyWallOutput() //
+            .withTimeline(ALICE, alicesTimeline) //
+            .withTimeline(CHARLIE, charliesTimeline) //
+            .create();
+
+        List<String> printedTimeline = output.print(PRINTING_TIME);
+
+        assertThat(printedTimeline, contains( //
+            CHARLIE + SEPERATOR + LATEST_MESSAGE + " (15 seconds ago)", //
+            ALICE + SEPERATOR + LATER_MESSAGE + " (1 minute ago)", //
+            CHARLIE + SEPERATOR + EARLIER_MESSAGE + " (2 minutes ago)", //
+            ALICE + SEPERATOR + EARLIEST_MESSAGE + " (5 minutes ago)"));
     }
 }
