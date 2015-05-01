062e43810a11b599bb1efc77c2bdff553a29fc91
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-05-01 15:21:55 +0200

    added test for multiple lines of output

--- src/main/java/application/SocialNetworkingConsole.java
+++ src/main/java/application/SocialNetworkingConsole.java
@@ -1,9 +1,14 @@
 package application;
 
+import java.util.List;
+
 public class SocialNetworkingConsole {
 
     public void print(String output) {
         System.out.print(output);
     }
 
+    public void print(List<String> output) {
+      
+    }
 }
--- src/test/java/application/SocialNetworkingConsoleTest.java
+++ src/test/java/application/SocialNetworkingConsoleTest.java
@@ -1,6 +1,7 @@
 package application;
 
 import static java.lang.System.setOut;
+import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,6 +13,7 @@ import org.junit.Test;
 public class SocialNetworkingConsoleTest {
 
     private static final String OUTPUT = "I love the weather today";
+    private static final String ANOTHER_OUTPUT = "Damn! We lost!";
 
     @Test
     public void givenAStringItPrintsThatStringToSystemOut() {
@@ -22,6 +24,16 @@ public class SocialNetworkingConsoleTest {
         console.print(OUTPUT);
 
         assertThat(actualOutput.toString(), is(OUTPUT));
+    }
 
+    @Test
+    public void givenMultipleStringsItPrintsThemLineByLineToSystemOut() {
+        ByteArrayOutputStream actualOutput = new ByteArrayOutputStream();
+        setOut(new PrintStream(actualOutput));
+        SocialNetworkingConsole console = new SocialNetworkingConsole();
+
+        console.print(asList(OUTPUT, ANOTHER_OUTPUT));
+
+        assertThat(actualOutput.toString(), is(OUTPUT + ANOTHER_OUTPUT));
     }
 }
