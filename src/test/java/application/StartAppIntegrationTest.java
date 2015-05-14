40025e3cf32e38603b29983585f5d3a92d3509d9
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-05-14 13:10:49 +0200

    changed integration test to use SystemRules

--- src/test/java/application/StartAppIntegrationTest.java
+++ src/test/java/application/StartAppIntegrationTest.java
@@ -1,31 +1,29 @@
 package application;
 
-import static java.lang.System.setOut;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
+import static org.junit.contrib.java.lang.system.LogMode.LOG_ONLY;
 import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
 import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 
 public class StartAppIntegrationTest {
     private static final String[] IRRELEVANT_PARAMETERS = null;
+
     @Rule
     public TextFromStandardInputStream input = emptyStandardInputStream();
+    @Rule
+    public final StandardOutputStreamLog log = new StandardOutputStreamLog(LOG_ONLY);
 
     @Test
     public void whenIStartTheAppItGreetsTheUser() {
-        ByteArrayOutputStream actualOutput = new ByteArrayOutputStream();
-        setOut(new PrintStream(actualOutput));
-
         StartApp.main(IRRELEVANT_PARAMETERS);
 
-        assertThat(actualOutput.toString(), containsString("Welcome to my social network application"));
+        assertThat(log.getLog(), containsString("Welcome to my social network application"));
     }
 
     @Test
@@ -35,12 +33,9 @@ public class StartAppIntegrationTest {
         String timelineCommand = "Alice";
         input.provideText(firstPost, timelineCommand);
 
-        ByteArrayOutputStream actualOutput = new ByteArrayOutputStream();
-        setOut(new PrintStream(actualOutput));
-
         StartApp.main(IRRELEVANT_PARAMETERS);
 
-        assertThat(actualOutput.toString(), containsString("I love the weather today ("));
+        assertThat(log.getLog(), containsString("I love the weather today ("));
     }
 
 }
