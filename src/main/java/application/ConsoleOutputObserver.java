ce9fe5235648cf00a1b5e55c20be41ea2b9e8e39
Franziska Sauerwein <franziska.sauerwein@gmail.com>
2015-05-14 17:44:32 +0200

    Added concept of ConsoleOutputObserver

---
+++ src/main/java/application/ConsoleOutputObserver.java
@@ -0,0 +1,17 @@
+package application;
+
+import time.SocialTimeClock;
+import timeline.Output;
+
+public class ConsoleOutputObserver {
+
+    public ConsoleOutputObserver(SocialNetworkingConsole console, SocialTimeClock clock) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void update(Output output) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
